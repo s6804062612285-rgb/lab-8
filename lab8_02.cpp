0a4e2b769c96dadff29dd21eb7199fa28c8b2800
s6804062612285 Warakorn <s6804062612285@email.kmutnb.ac.th>
2026-03-06 03:52:52 +0700

    Commit 4 >> Lab8no2.c: show no. of hardest no.

--- lab8_02.cpp
+++ lab8_02.cpp
@@ -4,8 +4,9 @@ int checkscore(char std[]);
 char charkeys[10] = {'D', 'B', 'D', 'C', 'C', 'D', 'A', 'E', 'A', 'D'};
 
 int main() {
-    int i;
+    int i, j;
     int count_no1 = 0;
+    int wrong_count[10] = {0};
     char ans[8][10] = {
         {'A','B','A','C','C','D','E','E','A','D'},
         {'D','B','A','B','C','A','E','E','A','D'},
@@ -19,12 +20,22 @@ int main() {
 
     for (i = 0; i < 8; i++) {
         printf("std %d => %d\n", i + 1, checkscore(ans[i]));
-        if (ans[i][0] == charkeys[0]) {
-            count_no1++;
+        if (ans[i][0] == charkeys[0]) count_no1++;
+        for (j = 0; j < 10; j++) {
+            if (ans[i][j] != charkeys[j]) {
+                wrong_count[j]++;
+            }
         }
     }
-    
     printf("\nTotal correct for 1: %d\n", count_no1);
+
+    int hardest_no = 0;
+    for (i = 1; i < 10; i++) {
+        if (wrong_count[i] > wrong_count[hardest_no]) {
+            hardest_no = i;
+        }
+    }
+    printf("Hard question is No.%d\n", hardest_no + 1, wrong_count[hardest_no]);
 
     return 0;
 }
